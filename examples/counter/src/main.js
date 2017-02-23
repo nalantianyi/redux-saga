d7435b3b527a0a447be3f58508d41680c5cff2b4
纳兰天忆 <103520146@qq.com>
2017-02-23 13:31:42 +0800

    //

--- examples/counter/src/main.js
+++ examples/counter/src/main.js
@@ -3,7 +3,7 @@ import "babel-polyfill"
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import sagaMonitor from '../../sagaMonitor'
 
@@ -14,23 +14,22 @@ import rootSaga from './sagas'
 
 const sagaMiddleware = createSagaMiddleware({sagaMonitor})
 const store = createStore(
-  reducer,
-  applyMiddleware(sagaMiddleware)
+    reducer,
+    applyMiddleware(sagaMiddleware)
 )
 sagaMiddleware.run(rootSaga)
 
 const action = type => store.dispatch({type})
 
 function render() {
-  ReactDOM.render(
-    <Counter
-      value={store.getState()}
-      onIncrement={() => action('INCREMENT')}
-      onDecrement={() => action('DECREMENT')}
-      onIncrementIfOdd={() => action('INCREMENT_IF_ODD')}
-      onIncrementAsync={() => action('INCREMENT_ASYNC')} />,
-    document.getElementById('root')
-  )
+    ReactDOM.render(
+        <Counter value={store.getState()}
+                 onIncrement={() => action('INCREMENT')}
+                 onDecrement={() => action('DECREMENT')}
+                 onIncrementIfOdd={() => action('INCREMENT_IF_ODD')}
+                 onIncrementAsync={() => action('INCREMENT_ASYNC')}/>,
+        document.getElementById('root')
+    )
 }
 
 render()
